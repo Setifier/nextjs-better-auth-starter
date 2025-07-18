d5b6799e8466a9fb1fafbdb5283d28cba576af70
Slim Etifier <73553076+Setifier@users.noreply.github.com>
2025-07-18 16:27:56 +0200

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center h-screen">
       <div className="flex flex-col items-center justify-center gap-8">
-        <h1 className="text-4xl font-bold">
-          Welcome to Better Auth with Next.js & Prisma Starter
+        <h1 className="text-4xl font-bold text-center">
+          Welcome to Better Auth <br /> with Next.js & Prisma Starter
         </h1>
         <GetStartedButton />
       </div>
